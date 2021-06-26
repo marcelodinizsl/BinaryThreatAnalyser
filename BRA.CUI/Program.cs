fb59cb78ecf2f15ad12c1176f43aa8600df599ba
marcelodinizsl <marcelodinizsl@gmail.com>
2021-06-26 10:31:02 -0300

    Including File checking and Virus Total checking

--- BRA.CUI/Program.cs
+++ BRA.CUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using BTA.Core;
 
@@ -8,12 +9,68 @@ namespace BRA.CUI
     {
         static void Main(string[] args)
         {
-            string path = @"c:\Temp\P1.jpg";
+            Console.WriteLine("BINARY THREAT ANALYSER TOOL - BTAT\n");
+            CheckFile(args);
+            StartScan(args[0]);
+        }
+
+        private static void StartScan(string filePath)
+        {
+            Console.WriteLine("Starting scannig .... ");
+            AnalyseFile(filePath);
+            Console.WriteLine("\nHave a nice day ");
+        }
+
+        private static void CheckFile(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("File needed to scan");
+                Console.WriteLine("Try Again");
+                Environment.Exit(0);
+            }
+
+            FileInfo fileInfo = new FileInfo(args[0]);
+
+            if (!fileInfo.Exists)
+            {
+                Console.WriteLine("File needed to scan");
+                Console.WriteLine("Try Again");
+                Environment.Exit(0);
+            }
+        }
+
+        private static void AnalyseFile(string path)
+        {
+            bool hasResult = false;
 
             Core core = new Core();
-            Task<String> result = core.VirusScanFile(path);
+            do
+            {
+                try
+                {
+                    Task<String> result = core.VirusScanFile(path);
+                    result.Wait();
 
-            Console.WriteLine(result.Result);
+                   Console.WriteLine("Printing the result");
+                   Console.WriteLine(result.Result);
+                   hasResult = true;
+                }
+                catch (IOException exception)
+                {
+                    Console.WriteLine("Error in the selected file.");
+                    Console.WriteLine(exception.Message);
+                    break;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Generic error.");
+                    Console.WriteLine(exception.Message);
+                    break;
+                }
+            } while (!hasResult);
+
+            
         }
     }
 }
--- BTA.Core/Core.cs
+++ BTA.Core/Core.cs
@@ -1,53 +1,72 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using VirusTotalNet;
-using VirusTotalNet.Exceptions;
+using VirusTotalNet.ResponseCodes;
 using VirusTotalNet.Results;
 
 namespace BTA.Core
 {
     public class Core
     {
+        static string API_KEY = "4636f6893f090d0204bfff61651e544fd38882ef4ddc591ca358d269cf520821";
+        static int SLEEP_TIME = 25000;
+        static int SLEEP_TIME_FROM_SCAN = 3000;
+
         public async Task<string> VirusScanFile(string filePath)
         {
-            StringBuilder sb = new StringBuilder();
+            VirusTotal virusTotal = GetVirusTotalInstance();
+            FileInfo fileInfo = new FileInfo(filePath);
+            FileReport finalReport = await CheckFileReport(virusTotal, fileInfo);
 
-            VirusTotal virusTotal = new VirusTotal("4636f6893f090d0204bfff61651e544fd38882ef4ddc591ca358d269cf520821")
+            if (finalReport != null)
+            {
+                StringBuilder resultInfo = BuildFinalReport(finalReport);
+
+                return resultInfo.ToString();
+            }
+
+            throw new Exception("ERROR: Problem in the scanning");
+        }
+
+        private VirusTotal GetVirusTotalInstance()
+        {
+            return new VirusTotal(API_KEY)
             {
                 UseTLS = true
             };
+        }
 
-            FileInfo fileInfo = new FileInfo(filePath);
+        private static StringBuilder BuildFinalReport(FileReport finalReport)
+        {
+            StringBuilder resultInfo = new StringBuilder();
+            resultInfo.AppendLine("Scan Id: " + finalReport.ScanId);
+            resultInfo.AppendLine("Scan date: " + finalReport.ScanDate);
+            resultInfo.AppendLine("Reports positives: " + finalReport.Positives);
+            resultInfo.AppendLine("Total of anti-virus usded in the analyse: " + finalReport.Total);
+            resultInfo.AppendLine("Final message: " + finalReport.VerboseMsg);
+        
+            return resultInfo;
+        }
 
-            if (!fileInfo.Exists)
-                return "ERROR: File dont exist";
-
-            //Check if the file has been scanned before.
+        private static async Task<FileReport> CheckFileReport(VirusTotal virusTotal, FileInfo fileInfo)
+        {
             FileReport report;
+            await virusTotal.ScanFileAsync(fileInfo);
+            Thread.Sleep(SLEEP_TIME_FROM_SCAN);
 
             do
             {
                 report = await virusTotal.GetFileReportAsync(fileInfo);
 
-                Thread.Sleep(3000);
+                if (report.ResponseCode != FileReportResponseCode.Present)
+                    Thread.Sleep(SLEEP_TIME);
 
-            } while (report.ResponseCode != VirusTotalNet.ResponseCodes.FileReportResponseCode.Present);
+            } while (report.ResponseCode != FileReportResponseCode.Present);
 
-
-            if (report != null)
-            {
-                sb.AppendLine("Reports positives: " + report.Positives);
-                sb.AppendLine("Total of anti virus checks" + report.Total);
-                sb.AppendLine("Final message" + report.VerboseMsg);
-
-                return sb.ToString();
-            }
-
-            return "Error:";
+            return report;
         }
     }
 }
